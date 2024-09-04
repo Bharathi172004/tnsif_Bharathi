750c9b58e36a5d2d1d294cd8e7b480590286a59e
bharathibharu1517@gmail.com <Lenovo@DESKTOP-UB2QKT8>
2024-09-04 10:18:21 +0530

    commit 1

---
+++ Main/src/com/tnsif/day9/Interface/Wrapperclass/Interface.java
@@ -0,0 +1,23 @@
+ package com.tnsif.day9.Interface.Wrapperclass;
+
+ 
+	interface one{
+		int age=20;
+		public void show();
+	}
+
+	class two implements one{
+		@Override
+		public void show() {
+			System.out.println("Interface method one");
+			System.out.println("Age:"+age);
+		}
+	}
+	public class InterfaceDemo {
+
+		public static void main(String[] args) {
+			two obj=new two();
+			obj.show();
+		}
+
+	}
